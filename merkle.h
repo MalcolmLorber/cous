901a4d15af7287573d9ec416a5e5f4d8479aa996
Malcolm Lorber <malcolmlorber@gmail.com>
2019-03-16 16:40:05 -0400

    typos

--- merkle.h
+++ merkle.h
@@ -1,10 +1,12 @@
 #pragma once
-#include <boost/multiprecision/cpp_int>
+#include <boost/multiprecision/cpp_int.hpp>
 #include "consts.h"
 #include <iostream>
 
+using boost::multiprecision::uint256_t;
+
 class MerkleStack {
-  uint256_t [64] stack;
+  uint256_t stack[64];
   uint64_t used;
   uint8_t buffer [1 + SegmentSize];
  public:
